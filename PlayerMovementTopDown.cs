c094d7699bb949e7a4a14ae3aace24c2529fb346
funkykawing <funkykawing@gmail.com>
2020-08-11 15:23:25 +0800

    Unity 2D top down player movement
    Got these on YouTube tutorial about the C# code of 2D top down player movement in Unity.

---
+++ PlayerMovementTopDown.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class PlayerMovementTopDown : MonoBehaviour
+{
+    private Animator myAnimator;
+    private Rigidbody2D rb2d;
+    
+    public float moveSpeed = 5.0f;
+    public Vector2 moveDirection;
+    
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        move();
+    }
+
+    private void ProcessInputs()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    private void move()
+    {
+        rb2d.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+}
